b242f84c036a9fedfce3e6a25b20b8e7ddc8a392
Devin Panikkar <dkpanikkar@comcast.net>
2019-09-16 14:27:56 -0600

    Update index.php

--- public_html/bacs350/superhero/index.php
+++ public_html/bacs350/superhero/index.php
@@ -12,13 +12,14 @@
     $site_title = 'UNC BACS 350';
     $page_title = 'Superhero Gallery';
 
-    $card1 = render_card("Captain America", "Classic America hero archtype");
-    $card2 = render_card("Iron Man", "Billionaire Entrpreneur");
+    $card1 = render_card("Daredevil", "After getting blinded at a young age by radiactive waste he learned to use his other senses to be able to act as a lawyer during the day and be a vigilante at night");
+    $card2 = render_card("Black Panther", "King of the advanced society of Wakanda acting as the Black Panthar to protect his people");
+    //d3 = render_card("Wolverine, A Mutant who had adamantime infused into his bones given him the abillity to push his hones out of his skin like claws to fight");
 
     $content =  '
         <div class="container-fluid">
             <div class="row">
-                ' . $card1 . $card2 . '
+                ' . $card1 . $card2 .3
             </div>
         </div>
     ';
