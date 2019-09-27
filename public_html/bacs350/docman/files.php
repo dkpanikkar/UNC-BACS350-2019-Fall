ced99f6869d2edff7ce0f74a3d8a94503dd101c2
Devin Panikkar <dkpanikkar@comcast.net>
2019-09-27 15:26:27 -0600

    Create files.php

---
+++ public_html/bacs350/docman/files.php
@@ -0,0 +1,31 @@
+<?php
+    //get_file_list
+    function get_file_list($path){
+        $files = array();
+        if (!is_dir($path)) {return $files; }
+
+        $items = scandir($path);
+        foreach ($items as $item) {
+            $item_path = $path . DIRECTORY_SEPERATOR . $item;
+            if (is_file($item_path)) {
+                $files[] = $item_path;
+            }
+        }
+        return $files;
+    }
+
+    //get_dir_list
+    function get_dir_list($path){
+        $files = array();
+        if (!is_dir($path)) {return $files; }
+        
+        $items = scandir($path);
+        foreach ($items as $$item) {
+            $item_path = $path . DIRECTORY_SEPERATOR . $item;
+            if (is_dir($item_path)) {
+                $files[] = $item_path;
+            }
+        }
+        return $files;
+    }
+?>
