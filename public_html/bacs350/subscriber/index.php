7508134ecf0ce1e791129d58fa70fcef080c1b81
Devin Panikkar <dkpanikkar@comcast.net>
2019-10-09 15:17:53 -0600

    Update index.php

--- public_html/bacs350/subscriber/index.php
+++ public_html/bacs350/subscriber/index.php
@@ -1,24 +1,24 @@
-<h1>Subscriber Database</h1>
+<h1>Subscribers Database - Step 2</h1>
 
 <p>This page demonstrates a connection to an actual database at Bluehost.</p>
+<p>The last output for this page should be a success confirmation. </p>
 
-<h2>Step 1 - Simple page</h2>
 
-<p>
-    <b></b> Start by building and debugging a page that looks like this one.
-    Get this working first!
-</p>
-<p>
-    <a href="step1.php">Subscribers Page</a>
-</p>
+<?php
 
-<h2>Step 2 - Normal page</h2>
+    // Connect to the subscribers database at Bluehost
+    require 'subscriber.php';
 
-<p>
-    The simple solution will duplicate a lot of code
-    on a real website so it is not suitable for production websites. Build functions
-    for all of the key operations that should be done.
-</p>
-<p>
-    <a href="step2.php">Subscribers Page</a>
-</p>
+
+    // Get a list of subscribers records
+    $subscribers = query_subscribers($db);
+
+
+    // Build a list of subscribers in HTML
+    $list = render_subscriber_list($subscribers);
+
+    echo $list;
+    
+?>
+
+<p><b>Success !!</b></p>
