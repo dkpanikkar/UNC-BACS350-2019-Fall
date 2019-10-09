18f7d6988187b7a9d381cb05fb9da6228bc3096f
Devin Panikkar <dkpanikkar@comcast.net>
2019-10-09 12:14:52 -0600

    Update doc.php

--- public_html/bacs350/docman/doc.php
+++ public_html/bacs350/docman/doc.php
@@ -1,24 +1,28 @@
 <?php
-    //get the parameter off the URL
-    //URL = http://localhost/bacs350/planner/doc.php?doc=ToDo.md
-    //Eg. Doc = ToDo.md
+
+    // Get the parameter off the URL
+    // URL = http://localhost/bacs350/planner/doc.php?doc=ToDo.md
+    // Eg. Doc = ToDo.md
 
     $doc = $_GET['doc'];
     $page = $_SERVER['REQUEST_URI'];
     $doc_label = "<p>" . $doc . "</p>";
 
-    //use the views code
+
+    // Use the views code
     require_once 'views.php';
 
-    //Link to Home
-    $home = '<a href= ".">Other Documents</a>';
 
-    //Read Markdown Text from file
+    // Link to Home
+    $home = '<a href=".">Other Documents</a>';
+
+
+    // Read Markdown Text from file
     $text = render_markdown($doc);
-    $content = render_card($doc, $text);    
-    
+    $content = render_card($doc, $text);
 
-//Display the HTML in the page
-echo render_page('Seaman Notes', "Documents Manager", $home . $content);
+
+    // Display the HTML in the page
+    echo render_page('Seaman Notes', "Document Manager", $home . $content);
 
 ?>
