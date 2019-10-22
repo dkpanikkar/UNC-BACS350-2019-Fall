45711a28ca7be153803c1668812794672e4d619d
Devin Panikkar <dkpanikkar@comcast.net>
2019-10-22 13:45:51 -0600

    Update index.php

--- public_html/bacs350/project/index.php
+++ public_html/bacs350/project/index.php
@@ -23,6 +23,21 @@
             <li>
                 <a href="02">Project #2 - Page Template</a>
             </li>
+            <li>
+                <a href="03">Project #3 - Superhero Gallary</a>
+            </li>
+             <li>
+                <a href="04">Project #4 - Planner</a>
+            </li>
+            <li>
+                <a href="05">Project #5 - Document Manager</a>
+            </li>
+            <li>
+                <a href="06">Project #6 - Subscriber Database</a>
+            </li>
+            <li>
+                <a href="07">Project #7 - Superhero Database</a>
+            </li>
         </ul>
     ';
 
