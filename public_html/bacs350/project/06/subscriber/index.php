66216edc48da4c62c7ecdd7e963ed15ab3d1177a
Devin Panikkar <dkpanikkar@comcast.net>
2019-10-22 13:45:06 -0600

    Create index.php

---
+++ public_html/bacs350/project/06/subscriber/index.php
@@ -0,0 +1,37 @@
+<!--
+<h1>Subscribers Database</h1>
+
+<p>This page demonstrates a connection to an actual database at Bluehost.</p>
+<p>The last output for this page should be a success confirmation. </p>
+-->
+
+ <link rel="stylesheet" href="style.css">
+
+<?php
+
+    // Connect to the subscribers database at Bluehost
+    require 'subscriber.php';
+    require 'views.php';
+    
+
+    // Card with subscribers
+    $title = 'Subscriber List';
+    $body = $list;
+    $card = render_card($title, $body);
+    // Get a list of subscribers records
+    $subscribers = query_subscribers($db);
+
+
+    // Build a list of subscribers in HTML
+    $list = render_subscriber_list($subscribers);
+
+
+    
+    echo render_page('Panikkar Notes', "Subscriber's", $card);
+    echo $list;
+
+
+    
+?>
+
+<p><b>Success !!</b></p>
